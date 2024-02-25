34665e23d46afb6bd7d4fc42190b8a36b2225a3a
zhenghe <zhenghe@kaopuyun.com>
2024-02-25 21:40:17 +0800

    update nvim config

--- nvim/lua/config/lazygit.lua
+++ nvim/lua/config/lazygit.lua
@@ -1,5 +1,5 @@
 utils.nmap('<C-g>', ':LazyGit<CR>')
 vim.g.lazygit_floating_window_winblend = 0
 vim.g.lazygit_floating_window_scaling_factor = 1.0
-vim.g.lazygit_floating_window_corner_chars = { '╭', '╮', '╰', '╯' }
+vim.g.lazygit_floating_window_border_chars = { '╭', '╮', '╰', '╯' }
 vim.g.lazygit_use_neovim_remote = 1
