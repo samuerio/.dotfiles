2f8af09f64b965280187cb85eb86b8129cd7edb4
samuerio <853713419@qq.com>
2024-10-07 19:46:55 +0800

    update ranger

--- ranger/commands.py
+++ ranger/commands.py
@@ -63,12 +63,12 @@ class fzf_select(Command):
         import os.path
         if self.quantifier:
             # match only directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            command = "find -L . \\( -path '*/\\.*' -o -fstype 'dev' -o -fstype 'proc' \\) -prune \
             -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
 
         else:
             # match files and directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            command = "find -L . \\( -path '*/\\.*' -o -fstype 'dev' -o -fstype 'proc' \\) -prune \
             -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
 
         fzf = self.fm.execute_command(command,
